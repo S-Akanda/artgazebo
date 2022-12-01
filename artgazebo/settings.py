936cf2836626a196acd9d182e48f1fa3197a3ee9
S-Akanda <stevebiko708@gmail.com>
2022-12-01 14:00:20 +0300

    new changes

--- artgazebo/settings.py
+++ artgazebo/settings.py
@@ -25,6 +25,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #my apps
+    'core',
+    'vendor',
 ]
 
 MIDDLEWARE = [
