682d2f40c4b11c78706f0bf3d497d83dd78b0c65
S-Akanda <stevebiko708@gmail.com>
2023-06-19 00:16:22 +0300

    bingo

--- artgazebo/settings.py
+++ artgazebo/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yf7e&mc+wy1ha53#d*rs8-6(c&b$9ih(v(!98!=x4&r$&6!s(h'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
